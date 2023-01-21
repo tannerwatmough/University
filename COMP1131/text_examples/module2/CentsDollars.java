82622cd73874a6e76f30b18769c3a145c21512c6
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-21 00:59:45 -0700

    Created Cents to Dollars program
    Takes in user inputted cents and converts it into dollars and cents.

---
+++ COMP1131/text_examples/module2/CentsDollars.java
@@ -0,0 +1,27 @@
+// ****************************************************************************
+// CentsDollars.java    Java Foundations
+//
+// Converts cents to dollars and cents.
+// ****************************************************************************
+
+import java.util.Scanner;
+
+public class CentsDollars {
+  // Calculate fuel efficiency based on values entered by user
+  public static void main(String[] args) {
+    int cents, dollars;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Input the cents: ");
+    cents = scan.nextInt();
+
+    dollars = cents / 100; // 100 cents per dollar
+    cents = cents % 100; // Remainder of cents
+
+    System.out.println("That is " + dollars + " dollars and " + cents +
+        " cents.");
+
+    scan.close();
+  }
+}
