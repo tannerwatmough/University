fc8588764654033cf7e1138f10e14ec7f99a56c5
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-21 01:13:07 -0700

    Crated a program that calculates coin change
    Takes the change (in cents) and converts it into the correct
    number of twoonies, loonies, quarters, dimes, nickels, and pennies.
    
    Note: twoonies are the Canadian $2 coin and loonies are the Canadian
    $1 coin.

---
+++ COMP1131/text_examples/module2/Change.java
@@ -0,0 +1,42 @@
+// ****************************************************************************
+// Change.java    Java Foundations
+//
+// Calculates change.This is where they try to fit dollars into the amount back,
+// then quarters, then dimes, then nickels, etc. 
+// ****************************************************************************
+
+import java.util.Scanner;
+
+public class Change {
+  // Calculate fuel efficiency based on values entered by user
+  public static void main(String[] args) {
+    int change, twoonies, dollars, quarters, dimes, nickels, cents;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter change in cents:");
+    change = scan.nextInt();
+
+    twoonies = change / 200;
+    change = change % 200;
+
+    dollars = change / 100;
+    change = change % 100;
+
+    quarters = change / 25;
+    change = change % 25;
+
+    dimes = change / 10;
+    change = change % 10;
+
+    nickels = change / 5;
+    change = change % 5;
+
+    cents = change;
+
+    System.out.println("Your change is: " + twoonies + " twoonies, " + dollars +
+        " loonies, " + quarters + " quarters, " + dimes + " dimes, " + nickels +
+        " nickels, and " + cents + " cents.");
+    scan.close();
+  }
+}
