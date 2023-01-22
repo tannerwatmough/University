5ba612df506623032c9d596f1a5e7c9d0d86ec6d
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-21 19:10:52 -0700

    Checks text files columns for space
    Checks column 10 of a text file to see if there is a space at
    column 10 and if the line is longer than 10 characters. If so
    it will print out the line number and the text file line that
    is greater than 10 or doesn't have a space there.

---
+++ COMP1131/text_examples/module3/ColumnCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+
+class ColumnCheck {
+  public static void main(String[] args) {
+    final int colNum = 10;
+    int counter = 0;
+    String line = null;
+    Scanner scan = new Scanner(System.in);
+
+    while (scan.hasNext()) {
+      line = scan.nextLine();
+      counter = counter + 1;
+      if (line.length() > colNum && line.charAt(colNum) != ' ')
+        System.out.println(counter + ":\t" + line);
+    }
+  }
+}
