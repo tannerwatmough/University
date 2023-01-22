0acf44abb4482be2b905bb65833817b3c68fb041
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-21 20:02:52 -0700

    Simple demo of char value limits

---
+++ COMP1131/text_examples/module2/CharAssassination.java
@@ -0,0 +1,14 @@
+class CharAssassination {
+  public static void main(String[] args) {
+    char ch = 'A';
+    System.out.println("A char: " + ch);
+  }
+}
+
+// char is 16 bits of main memory to represent characters.
+
+// 'Z' prints out Z.
+// 'AA'has unclosed character literal compiler error.
+// ' ' prints out a space
+// '' compiler error for empty character literal.
+// "A" compiler error for incompatible types: String cannot be converted to char
