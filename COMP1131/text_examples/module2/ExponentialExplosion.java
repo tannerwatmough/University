38d66442232b53fcb3aa0a413f1048f2672bd220
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-21 19:58:52 -0700

    Exponent test on double limit

---
+++ COMP1131/text_examples/module2/ExponentialExplosion.java
@@ -0,0 +1,10 @@
+class ExponentialExplosion {
+  public static void main(String[] args) {
+    double value = 709;
+    System.out.println("e to the power value: " + Math.exp(value));
+  }
+}
+
+// 32 as initial value is fine.
+// Eventually outputs Infinity instead of a number.
+// 700 is okay
