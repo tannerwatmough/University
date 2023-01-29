e1357dece4b87c364751c839e07607805cff3b79
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-28 20:04:53 -0700

    Sphere Values Calculator
    Takes a radius from user and calculates the surface area, volume, and
    circumference and displays it to the console.

--- TRU/winter2023/COMP1131/assignments/assignment2/Sphere.java
+++ TRU/winter2023/COMP1131/assignments/assignment2/Sphere.java
@@ -1,39 +1,61 @@
 /* ===========================================================================
- * Fractions.java
+ * Sphere.java
  * 
  * Tanner Watmough T00712721
- * COMP 1131 Assignment 1 Question 3
- * This application prompts the user for a numerator and denominator of a
- * fraction as integers, then displays the decimal equivalent of the fraction.
+ * COMP 1131 Assignment 2 Question 3
+ * This application reads the radius of a sphere and calculates and displays
+ * the circumference, volume, and surface area. 
+ * 
+ * Where r is radius:
+ * Circumference = 2 * PI * r
+ * Volume = (4.0/3.0) * PI * Math.pow(r, 3)
+ * Surface Area = 4 * PI * Math.pow(r, 2)
  * ===========================================================================
  */
 
- import java.util.Scanner;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
- public class Fractions {
- 
-   public static void main(String[] args) {
-     int numerator, denominator;
-     double decimal;
- 
-     // Prompts user for pound value
-     Scanner scan = new Scanner(System.in);
-     System.out.println("Fractions to Decimal Converter ");
-     System.out.print("Please enter the numerator: ");
-     numerator = scan.nextInt();
-     System.out.print("Please enter the denominator: ");
-     denominator = scan.nextInt();
-     System.out.println();
- 
-     /*
-      * Converts the fraction to a decimal. Casting required to cue floating
-      * point division instead of integer division.
-      */
-     decimal = (double) numerator / denominator;
- 
-     // Display Results
-     System.out.println(numerator + "/" + denominator + " is equal to " + decimal);
- 
-     scan.close();
-   }
- }
+public class Sphere {
+
+  public static void main(String[] args) {
+    double radius = 1, surfArea, volume, circumference;
+    boolean prompt = true;
+
+    Scanner scan = new Scanner(System.in);
+    /*
+     * Formatter to limit fractional portion to at most 3 places.
+     * Also adds thousands place separators for large values.
+     */
+    DecimalFormat fmt = new DecimalFormat("#,###,##0.###");
+
+    System.out.println("Values of a Sphere");
+
+    // Prompt user for radius. Keeps prompting until positive radius given.
+    while (prompt) {
+      System.out.println("Please enter a positive radius: ");
+      radius = scan.nextDouble();
+      if (radius > 0) {
+        prompt = false;
+      }
+    }
+
+    System.out.println();
+
+    // Calculate circumference of sphere
+    circumference = 2 * Math.PI * radius;
+
+    // Calculate volume of sphere. Integer division causes significant errors.
+    volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+    // Calculate surface area of sphere
+    surfArea = 4 * Math.PI * Math.pow(radius, 2);
+
+    // Display Results
+    System.out.println("Circumference: \t" + fmt.format(circumference));
+    System.out.println("Volume: \t" + fmt.format(volume));
+    System.out.println("Surface Area: \t" + fmt.format(surfArea));
+
+    scan.close();
+  }
+}
