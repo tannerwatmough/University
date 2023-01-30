1b64590a329231e8d7e275b0ec7dca8cc1592f1f
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-01-29 23:41:31 -0700

    Little ASCII ghost

---
+++ TRU/winter2023/COMP1131/practice/Ghost.java
@@ -0,0 +1,30 @@
+public class Ghost {
+  public static void main(String[] args) {
+    System.out.println("      ###,  ,##, ,##,");
+    System.out.println("       #  # #  # #  #");
+    System.out.println("       ###  #  # #  #");
+    System.out.println("       #  # #  # #  #");
+    System.out.println("       ###' '##' '##'");
+    System.out.println("            .--,");
+    System.out.println("           /  (");
+    System.out.println("          /    \\");
+    System.out.println("         /      \\");
+    System.out.println("        /  0  0  \\");
+    System.out.println("((()   |    ()    |   ()))");
+    System.out.println("\\  ()  (  .____.  )  ()  /");
+    System.out.println(" |` \\_/ \\  `\"\"`  / \\_/ `|");
+    System.out.println(" |       `.'--'.'       |");
+    System.out.println("  \\        `\"\"'        /");
+    System.out.println("   \\                  /");
+    System.out.println("    `      BRADY     '");
+    System.out.println("     |              |     ,");
+    System.out.println("     |`             |  _.'|");
+    System.out.println("     |              `-'  /");
+    System.out.println("     \\                  '");
+    System.out.println("      `._____________.-'");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
+  }
+}
