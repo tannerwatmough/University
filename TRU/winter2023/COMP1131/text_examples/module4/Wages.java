3e7fc6103a65b2304a658dcb8af82d7958be31b2
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-03-23 15:49:09 -0600

    Added module 4 examples

---
+++ TRU/winter2023/COMP1131/text_examples/module4/Wages.java
@@ -0,0 +1,27 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Wages
+{
+  final double RATE = 8.25; // regular pay rate
+  final int STANDARD = 40; // standard hours in work week
+
+  Scanner scan = new Scanner(System.in);
+
+	double pay = 0.0;
+
+  System.out.print("Enter the number of hours worked: ");
+  int hours = scan.nextInt();
+
+  System.out.println();
+
+  // Pay overtime at "time and a half"
+
+  if (hours > STANDARD)
+	  pay = STANDARD * RATE + (hours-STANDARD) * (RATE * 1.5);
+  else
+    pay = hours * RATE;
+
+  NumberFormat fmt = NumberFormat.getCurrencyInstance();
+  System.out.println("Gross earnings: " + fmt.format(pay));
+}
