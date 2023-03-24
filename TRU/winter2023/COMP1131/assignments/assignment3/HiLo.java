6cbfc7361a15f0f3340b8b5cd81050483e15a94d
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-03-24 03:04:25 -0600

    Assignment 3 files

--- TRU/winter2023/COMP1131/assignments/assignment3/HiLo.java
+++ TRU/winter2023/COMP1131/assignments/assignment3/HiLo.java
@@ -1,26 +1,89 @@
+/* ===========================================================================
+ * HiLo.java
+ * 
+ * Tanner Watmough T00712721
+ * COMP 1131 Assignment 3 Question 1
+ * 
+ * This application plays a High-Low guessing game with the computer picking
+ * random numbers between 1 and 100 (inclusive), then prompting the user to 
+ * guess. It will prompt the user if their guess was high or low and let them
+ * quit at any point. 
+ * 
+ * The user will be allowed to guess until they get it right or quit. 
+ * 
+ * The number of guesses are counted unless they are out of range and they will 
+ * be told so. 
+ * 
+ * Guesses value reported to user when they get the correct answer.
+ * 
+ * At the end of each game, either when quitting or correctly guessing, prompt 
+ * the user if they wish to play again. 
+ * 
+ * ===========================================================================
+ */
+
 import java.util.*;
 
 public class HiLo {
-  // Plays a simple guessing game with user
   public static void main(String[] args) {
-    final int MAX = 10;
-    int answer, guess;
+    /*
+     * Idea of MAX taken from textbook example of Guessing. This is the more
+     * robust solution for easily changing program parameters.
+     */
+    final int MAX = 100;
+    int answer, guess, numOfGuesses;
+    String replay = "y";
 
     Scanner scan = new Scanner(System.in);
     Random generator = new Random();
 
-    answer = generator.nextInt(MAX) + 1;
+    // Lets lowercase or uppercase y restart the game.
+    while (replay.equalsIgnoreCase("y")) {
+      // Reset guesses to make nested while loop true again.
+      guess = -1;
 
-    System.out.print("I'm thinking of a number between 1 and " + MAX +
-        ". Guess what it is: ");
+      // Reset number of guesses to 0 for new games.
+      numOfGuesses = 0;
 
-    guess = scan.nextInt();
+      // Generates a random number between 0 and the MAX - 1.
+      answer = generator.nextInt(MAX) + 1;
 
-    if (guess == answer)
-      System.out.println("You got it! Good guessing!");
-    else {
-      System.out.println("That is not correct, sorry.");
-      System.out.println("The answer was " + answer);
+      System.out.print("I'm thinking of a number between 1 and " + MAX +
+          ". Guess what it is. Enter 0 to quit at anytime: ");
+
+      // Allows continuous guessing until answer is guessed or user quits.
+      while (guess != answer && guess != 0) {
+        guess = scan.nextInt();
+
+        // Presents number of guesses when user gets correct and prompts replay
+        if (guess == answer) {
+          System.out.println("You got it! Good guessing!");
+          numOfGuesses++;
+          System.out.println("You got the answer in " + numOfGuesses + " guesses.");
+        } // makes sure guess is less than the answer but not below 1.
+        else if (guess < answer && guess > 0) { //
+          System.out.println("The answer is higher than " + guess + "! Guess again.");
+          numOfGuesses++;
+        } // makes sure guess is greater than the answer but not above 100.
+        else if (guess > answer && guess <= 100) {
+          System.out.println("The answer is lower than " + guess + "! Guess again.");
+          numOfGuesses++;
+        } else if (guess == 0) {
+          // Required so the out of range message doesn't print.
+          System.out.println("Quitting round");
+        }
+        // Covers out of range guesses. numOfGuesses not incremented.
+        else {
+          System.out.println("Your guess was out of range. Please guess between 1" +
+              " and 100 or type 0 to quit.");
+        }
+
+      }
+
+      // Checks if user wishes to replay. Will replay if they enter 'y' or 'Y'
+      System.out.println("Would you like to play again? (y/n): ");
+
+      replay = scan.next();
     }
 
     scan.close();
--- TRU/winter2023/COMP1131/assignments/assignment3/VowelCounter.java
+++ TRU/winter2023/COMP1131/assignments/assignment3/VowelCounter.java
@@ -1,21 +1,52 @@
+/* ===========================================================================
+ * VowelCounter.java
+ * 
+ * Tanner Watmough T00712721
+ * COMP 1131 Assignment 3 Question 2
+ * 
+ * This reads a string from the user and determines how many of each of the five
+ * vowels appear in the string. It also counts and prints the number of non-vowel
+ * characters. 
+ * 
+ * Vowels: 'a', 'e', 'i', 'o', and 'u'
+ * ===========================================================================
+ */
+
 import java.util.Scanner;
 
 public class VowelCounter {
   public static void main(String args[]) {
-    String word;
+    String userString;
 
-    char letter;
-
-    int aCount = 0, eCount = 0, iCount = 0;
+    // Initialize counters for vowels and consonants
+    int aCount = 0, eCount = 0, iCount = 0, oCount = 0, uCount = 0;
+    int consonantCount = 0;
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.print("Please enter a word");
-    word = scan.next();
-    word = word.toUpperCase();
+    System.out.println("This will count each vowel individually and consonants"
+        + " together in an inputted String.");
+    System.out.println("Please enter a string:");
+    userString = scan.nextLine();
 
-    for (int i = 0; i < word.length(); i++) {
-      switch (word.charAt(i)) {
+    // Handles both uppercase and lowercase by transforming it all to uppercase
+    userString = userString.toUpperCase();
+    // System.out.println(userString); For testing.
+
+    /*
+     * This will replace all punctuation in line. Referenced from Stack Overflow.
+     * https://stackoverflow.com/questions/18830813/
+     * how-can-i-remove-punctuation-from-input-text-in-java
+     * This is done so users can enter multiple word strings without needing a
+     * case for each consonant. It uses regex to go through the string and turns
+     * all predefined punctuation to an empty string, effectively removing it.
+     */
+    userString = userString.replaceAll("\\p{Punct}", ""); // Remove to count
+                                                          // all characters.
+    // System.out.println(userString); For testing.
+
+    for (int i = 0; i < userString.length(); i++) {
+      switch (userString.charAt(i)) {
         case 'A':
           aCount++;
           break;
@@ -25,10 +56,34 @@ public class VowelCounter {
         case 'I':
           iCount++;
           break;
+        case 'O':
+          oCount++;
+          break;
+        case 'U':
+          uCount++;
+          break;
+        // Handles spaces
+        case ' ': // Could be removed if you wanted it counted in default.
+          break;
         default:
-          System.out.println("Error in Identification Character");
+          /*
+           * On rereading the assignment instructions, I know there's a possibility
+           * that you meant for me to count all characters (including punctuation).
+           * I'd simply fix that by renaming the variable below but still
+           * incrementing it and taking out the userString.replaceAll regex.
+           * If you wanted spaces counted too, I'd remove the case that handles
+           * them above.
+           */
+          consonantCount++;
+          break;
       }
     }
+
+    System.out.println();
+    System.out.println("Your string had: ");
+    System.out.println("A: " + aCount + "\t E: " + eCount + "\t I:" +
+        iCount + "\t O: " + oCount + "\t U: " + uCount + "\t Consonants: " +
+        consonantCount);
 
     scan.close();
   }
