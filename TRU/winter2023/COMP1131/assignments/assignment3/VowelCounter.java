0806f0e19e1e6eb288db101383d76bcc916bdf3e
Tanner <79290989+tannerwatmough@users.noreply.github.com>
2023-03-23 18:11:08 -0600

    Fixed unneeded if statement

---
+++ TRU/winter2023/COMP1131/assignments/assignment3/VowelCounter.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+  public static void main(String args[]) {
+    String word;
+
+    char letter;
+
+    int aCount = 0, eCount = 0, iCount = 0;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Please enter a word");
+    word = scan.next();
+    word = word.toUpperCase();
+
+    for (int i = 0; i < word.length(); i++) {
+      switch (word.charAt(i)) {
+        case 'A':
+          aCount++;
+          break;
+        case 'E':
+          eCount++;
+          break;
+        case 'I':
+          iCount++;
+          break;
+        default:
+          System.out.println("Error in Identification Character");
+      }
+    }
+
+    scan.close();
+  }
+}
